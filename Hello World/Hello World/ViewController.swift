82ec5f21e5b711b5386184490db68d31602ed4b9
Jantosh <bjantosh@gamil.com>
2018-09-08 20:23:07 -0700

    Hello Workd Project

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by user144392 on 9/3/18.
+//  Copyright © 2018 user144392. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
